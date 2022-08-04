57dc1f6e91cc0dffc007458f155348bd16ba1444
Быков Егор <SEEGa1234@gmail.com>
2022-08-04 15:11:17 +0300

    add request module

--- Controllers/CurrencyController.cs
+++ Controllers/CurrencyController.cs
@@ -4,16 +4,16 @@ namespace WebServiceCurrency.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class CurrencyController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ILogger<CurrencyController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public CurrencyController(ILogger<CurrencyController> logger)
         {
             _logger = logger;
         }
