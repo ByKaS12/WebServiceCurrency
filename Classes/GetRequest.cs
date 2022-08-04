f9a8d979863269e575ce15731976e5261dbec1c6
Egor <Egor@PC_EGOR>
2022-08-04 15:09:16 +0300

    Добавьте файлы проекта.

---
+++ Classes/GetRequest.cs
@@ -0,0 +1,31 @@
+﻿using System.Net;
+namespace WebServiceCurrency.Classes
+{
+    public class GetRequest
+    {
+        private HttpWebRequest _request;
+        public string _address;
+
+        public string Response { get; set; }
+        public GetRequest(string address)
+        {
+            _address = address;
+        }
+        public void Run()
+        {
+            _request = (HttpWebRequest)WebRequest.Create(_address);
+            _request.Method = "GET";
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)_request.GetResponse();
+                var stream = response.GetResponseStream();
+                if (stream != null)
+                {
+                    Response = new StreamReader(stream).ReadToEnd();
+                }
+
+            }
+            catch (Exception) { }
+        }
+    }
+}
